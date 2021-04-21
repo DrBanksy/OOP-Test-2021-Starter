54ffb32a7f76154fe1244c1ab19043c4aef83e0c
cormac smith <cormacsmith@live.co.uk>
2021-04-21 16:06:08 +0100

    added arraylist for notes

---
+++ java/src/ie/tudublin/Note.java
@@ -0,0 +1,18 @@
+package ie.tudublin;
+
+public class Note {
+    private char note;
+    private int duration;
+
+
+    public Note(char note, int duration) {
+        this.note = note;
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Note [duration=" + duration + ", note=" + note + "]";
+    }
+    
+}
